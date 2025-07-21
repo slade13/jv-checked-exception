058af85306a015ccb259b17b59d0b8ba0c512975
Mateusz Lubiński <mateuszlubinski@hotmail.com>
2025-07-21 14:29:39 +0200

    add user's password validation

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,14 @@
 package core.basesyntax;
 
-//write your code here
+public class PasswordValidationException extends Exception {
+    public PasswordValidationException() {
+    }
+
+    public PasswordValidationException(String message) {
+        super(message);
+    }
+
+    public PasswordValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
